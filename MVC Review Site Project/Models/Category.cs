9e61b0e8273d5ceeb586b887eeef49fe2b2d498b
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-07-13 14:51:42 -0400

    initial commit pre scaffold

---
+++ MVC Review Site Project/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Site_Project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string BrandName { get; set; }
+        public double Price { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+
+        
+    }
+}
---
+++ MVC Review Site Project/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Review_Site_Project.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewPrimaryID { get; set; }
+        public string UserTitle { get; set; }
+        public string Content { get; set; }
+        public string UserPublishDate { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
