1398a3d566ee3c0a64ab33de722034aaa081ad18
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-07-15 16:44:52 -0400

    some styling and nav bar links added

--- MVC Review Site Project/Models/Category.cs
+++ MVC Review Site Project/Models/Category.cs
@@ -7,11 +7,11 @@ using System.Web;
 
 namespace MVC_Review_Site_Project.Models
 {
-    public class Category
+    public class Category 
     {
         [Key]
         public int CategoryID { get; set; }
-        public string BrandName { get; set; }
+        public string BrandName { get; set; }//I created my controllers before I fixed my spacing, which is why it is showing up on the site like this. I don't want to make any changes here because I don't want to alter any other code.
         public double Price { get; set; }
         public virtual ICollection<Review> Reviews { get; set; }
 
